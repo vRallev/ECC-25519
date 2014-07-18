261306dbf6b214c00eb6b91339c3c79bf51225c2
Ralf Wondratschek <rwo@adorsys.de>
2014-07-18 18:18:31 +0200

    Add facade project

---
+++ ECC-25519-Android-Facade/src/main/java/net/vrallev/android/ecc/Ecc25519Helper.java
@@ -0,0 +1,98 @@
+package net.vrallev.android.ecc;
+
+import com.github.dazoe.android.Ed25519;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import djb.Curve25519;
+
+/**
+ * @author Ralf Wondratschek
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class Ecc25519Helper {
+
+    protected static final MessageDigest MESSAGE_DIGEST_SHA_256;
+
+    static {
+        try {
+            MESSAGE_DIGEST_SHA_256 = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            // ignore, won't happen
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private final KeyHolder mKeyHolder;
+
+    public Ecc25519Helper() {
+        this((KeyHolder) null);
+    }
+
+    public Ecc25519Helper(byte[] privateKey) {
+        this(new KeyHolder(privateKey));
+    }
+
+    public Ecc25519Helper(KeyHolder keyHolder) {
+        mKeyHolder = keyHolder;
+    }
+
+    /*
+     * Diffie Hellman
+     */
+
+    public byte[] diffieHellman() {
+        return diffieHellman(mKeyHolder.getPrivateKey(), mKeyHolder.getPublicKeyDiffieHellman());
+    }
+
+    public byte[] diffieHellman(byte[] privateKey, byte[] publicKey) {
+        byte[] sharedSecret = new byte[32];
+        Curve25519.curve(sharedSecret, privateKey, publicKey);
+
+        // see documentation of curve function above
+        MESSAGE_DIGEST_SHA_256.reset();
+        return MESSAGE_DIGEST_SHA_256.digest(sharedSecret);
+    }
+
+    /*
+     * Signature
+     */
+
+    public byte[] sign(byte[] message) {
+        return sign(message, mKeyHolder.getPrivateKey());
+    }
+
+    public byte[] sign(byte[] message, byte[] privateKey) {
+        try {
+            return Ed25519.Sign(message, privateKey);
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /*
+     * Signature verification
+     */
+
+    public boolean isValidSignature(byte[] message, byte[] signature) {
+        return isValidSignature(message, signature, mKeyHolder.getPublicKeySignature());
+    }
+
+    public boolean isValidSignature(byte[] message, byte[] signature, byte[] publicKey) {
+        try {
+            return 0 == Ed25519.Verify(message, signature, publicKey);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /*
+     * Getter
+     */
+
+    public KeyHolder getKeyHolder() {
+        return mKeyHolder;
+    }
+}
---
+++ ECC-25519-Android-Facade/src/main/java/net/vrallev/android/ecc/KeyHolder.java
@@ -0,0 +1,81 @@
+package net.vrallev.android.ecc;
+
+import com.github.dazoe.android.Ed25519;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import djb.Curve25519;
+
+/**
+ * @author Ralf Wondratschek
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class KeyHolder {
+
+    protected static final MessageDigest MESSAGE_DIGEST_SHA_256;
+
+    static {
+        try {
+            MESSAGE_DIGEST_SHA_256 = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            // ignore, won't happen
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static byte[] createPrivateKey(byte[] seed) {
+        MESSAGE_DIGEST_SHA_256.reset();
+        return MESSAGE_DIGEST_SHA_256.digest(seed);
+    }
+
+    protected final byte[] mPrivateKey;
+    protected final byte[] mPublicKeyDiffieHellman;
+    protected final byte[] mPublicKeySignature;
+
+    public KeyHolder(byte[] privateKey) {
+        if (privateKey == null || (privateKey.length != 32 && privateKey.length != 64)) {
+            throw new IllegalArgumentException("private key must contain 32 or 64 bytes.");
+        }
+
+        Curve25519.clamp(privateKey);
+
+        mPrivateKey = Arrays.copyOf(privateKey, privateKey.length);
+
+        mPublicKeyDiffieHellman = new byte[32];
+        Curve25519.keygen(mPublicKeyDiffieHellman, null, mPrivateKey);
+
+        mPublicKeySignature = computePublicSignatureKey(mPrivateKey);
+    }
+
+    public KeyHolder(byte[] publicKeyDiffieHellman, byte[] publicKeySignature) {
+        this(null, publicKeyDiffieHellman, publicKeySignature);
+    }
+
+    public KeyHolder(byte[] privateKey, byte[] publicKeyDiffieHellman, byte[] publicKeySignature) {
+        mPrivateKey = privateKey;
+        mPublicKeyDiffieHellman = publicKeyDiffieHellman;
+        mPublicKeySignature = publicKeySignature;
+    }
+
+    protected byte[] computePublicSignatureKey(byte[] privateKey) {
+        try {
+            return Ed25519.PublicKeyFromPrivateKey(privateKey);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public byte[] getPrivateKey() {
+        return mPrivateKey;
+    }
+
+    public byte[] getPublicKeyDiffieHellman() {
+        return mPublicKeyDiffieHellman;
+    }
+
+    public byte[] getPublicKeySignature() {
+        return mPublicKeySignature;
+    }
+}
